dcbfcf3ca5afa5053a92b81effb5f91a5bef8152
Joseph Samuel Paez Cobos <135568580+2236571@users.noreply.github.com>
2023-12-06 19:54:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
-
+from flask import Flask,render_template
+import requests
 app = Flask(__name__)
+
+def get_pokemon_data(pokemon):
+    
+    url=f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
+    r = requests.get(url).json()
+    return r
 
 @app.route("/")
 def home():
-    return "<p>Hello, World!</p>"
+    return render_template('pokemon.html')
+
+@app.route("/detalle")
+def detalle():
+    return render_template('detalle.html')
 
 
-if __name__ =='__main__s':
+if __name__=='__main__':
     app.run(debug=True)
