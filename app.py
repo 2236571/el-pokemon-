bf284f52b05cf19627e65cac0ec130f76336d90f
Joseph Samuel Paez Cobos <135568580+2236571@users.noreply.github.com>
2023-12-11 19:54:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 from flask import Flask,render_template
 import requests
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column  
+
+
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pokedex.sqlite"
+
+db = SQLAlchemy(app)
+
+class Pokemon(db.Model):
+    id :Mapped[int] = mapped_column(db.Integer,primary_key=True,autoincrement=True)
+    name: Mapped[str] = mapped_column(db.String,nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+
+
+
 
 def get_pokemon_data(pokemon):
     
@@ -15,6 +38,31 @@ def home():
 @app.route("/detalle")
 def detalle():
     return render_template('detalle.html')
+
+@app.route("/insert")
+def insert():
+    new_pokemon= 'Pikachu'
+    
+    if new_pokemon:
+        obj=Pokemon(name=new_pokemon)
+        db.session.add(obj)
+        db.session.commit()    
+       
+    return 'Pokemon agregado'
+
+
+@app.route("/select")
+def insert():
+    Lista_pokemon = Pokemon.query.all()
+    for p in Lista_pokemon:
+        print(p.name)
+    return 'alo'    
+
+
+@app.route("/select/<name>")
+def selectbyname(name):
+    poke = Pokemon.query.filter_by(name=name).first()
+    return str (poke.id)
 
 
 if __name__=='__main__':
